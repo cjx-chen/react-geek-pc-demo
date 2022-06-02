91fb2226c8ef031ed60e699f29d59af7a512da0b
chenjiaxi2 <chenjiaxi2@yy.com>
2022-06-02 10:32:14 +0800

    项目配置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+// 导入路由
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// 导入页面组件
+import Login from "./pages/Login";
+import Layout from "./pages/Layout";
+
+// 配置路由规则
 function App() {
-  return <div>根组件</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
